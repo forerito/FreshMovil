6743d3c55778bc30ffd1760da6698883256be0b0
Camilo Forero <forero5515@gmail.com>
2023-06-23 02:17:57 -0500

    Depronto

--- src/views/screens/DoctorCard.js
+++ src/views/screens/DoctorCard.js
@@ -147,7 +147,7 @@ const DoctorCard = ({ valoracion, votos, comentarios, data, ratingId }) => {
               <Image
                 source={{
                   uri:
-                    data.foto_perfil ||
+                    
                     "https://i.pinimg.com/564x/51/f6/fb/51f6fb256629fc755b8870c801092942.jpg",
                 }}
                 style={styles.doctorImage}
